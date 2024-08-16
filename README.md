4f6706c6c93b413bc581a7c9f6a43c272b0533ac
Pierre F. Aubert <1709343+pierreaubert@users.noreply.github.com>
2024-08-16 13:31:52 +0000

    fix speedtest execution, put credential in a vault with systemd-creds

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# scrapers
+# Prometheus scrapers
 
-various prometheus scrapers for my homelab (nvidia, amd gpu, etc)
+Various prometheus scrapers for my homelab (nvidia, amd gpu, etc)
+This is more for me than usable by others but it can serve as example.
+The CLI parsers are generic and call easily be reused.
 
 ## GPUs
 
@@ -29,3 +31,18 @@ via https://github.com/bonovoxly/coinmarketcap-exporter
 ## FoundationDB
 
 via https://hub.docker.com/r/aikoven/foundationdb-exporter
+
+## Script Exporter
+
+via https://github.com/ricoberger/script_exporter
+
+## HP SSA Cli
+
+`hpssacli` is still available from HP.
+I parsed the output via `script_exporter`.
+
+# Prometheus and Victoria configuration
+
+to be added
+
+
--- conf/script_exporter/speedtest.sh
+++ conf/script_exporter/speedtest.sh
@@ -1,5 +1,5 @@
-# https://github.com/billimek/prometheus-speedtest-exporter/blob/master/speedtest-exporter.sh
 #!/bin/bash
+# https://github.com/billimek/prometheus-speedtest-exporter/blob/master/speedtest-exporter.sh
 
 # printMetric name description type value server_id
 function printMetric {
@@ -18,7 +18,7 @@ then
         while IFS=$'\t' read -r servername serverid latency jitter packetloss download upload downloadedbytes uploadedbytes share_url; do
             printMetric "speedtest_latency_seconds" "Latency" "gauge" "$latency"
             printMetric "speedtest_jittter_seconds" "Jitter" "gauge" "$jitter"
-n            printMetric "speedtest_download_bytes" "Download Speed" "gauge" "$download"
+            printMetric "speedtest_download_bytes" "Download Speed" "gauge" "$download"
             printMetric "speedtest_upload_bytes" "Upload Speed" "gauge" "$upload"
             printMetric "speedtest_downloadedbytes_bytes" "Downloaded Bytes" "gauge" "$downloadedbytes"
             printMetric "speedtest_uploadedbytes_bytes" "Uploaded Bytes" "gauge" "$uploadedbytes"
@@ -37,3 +37,5 @@ else
         done < <(/snap/bin/speedtest --accept-license --accept-gdpr -f tsv --server-id $server_id)
     done
 fi
+
+exit 1
